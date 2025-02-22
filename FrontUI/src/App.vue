19a11595ce23d8c3d9c4e54391eb88e4706ff010
ezl <ericzliu@gmail.com>
2025-02-22 14:06:16 -0600

    remove default hello world

--- FrontUI/src/App.vue
+++ FrontUI/src/App.vue
@@ -1,20 +1,13 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+export default {
+  name: 'App'
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div style="height: 100vh; display: flex; justify-content: center; align-items: center;">
+    <h1>Hello, World.</h1>
+  </div>
 </template>
 
 <style scoped>
